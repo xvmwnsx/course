fb63f427f866a50e794fef75e7bfce4b5f2071e4
ksenia <kochetkovaksenia1@gmail.com>
2025-01-22 16:14:42 +0300

    перенесены данные с sqlite3 на бд postgresql с бд

--- student_infobase/student_infobase/settings.py
+++ student_infobase/student_infobase/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'student_infobase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'D:/sqlite_3/students.db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'students_infobase',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
     }
 }
 
